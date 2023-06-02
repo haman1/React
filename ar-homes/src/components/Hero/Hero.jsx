e1be3a71fb2482ebf7d14c56e2ae5a760735b6eb
ombasa <ombasaherman@gmail.com>
2023-06-02 22:58:07 +0300

    Hero section

---
+++ ar-homes/src/components/Hero/Hero.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import {HiLocationMarker} from 'react-icons/hi';
+import './Hero.css'
+import CountUp from 'react-countup';
+
+function Hero() {
+  return (
+    <section className='hero-wrapper'>
+        <div className="paddings innerWidth flexCenter hero-container">
+
+            <div className="flexColStart hero-left">
+                <div className="hero-title">
+                    <div className="orange-circle" />
+                    <h1>
+                        Discover<br></br>
+                        Most Suitable<br></br>
+                        Property
+                    </h1>
+                </div>
+
+                <div className="flexColStart hero-des">
+                    <span className='secondaryText'>Find a variety of properties that suit you very easilty</span>
+                    <span className='secondaryText'>Forget all difficulties in finding a residence for you</span>
+                </div>
+
+                <div className="flexCenter search-bar">
+                    <HiLocationMarker color="var(--blue)" size={25}/>
+                    <input type="text" />
+                    <button className="button">Search</button>
+                </div>
+
+                <div className="flexCenter stats">
+                    <div className="flexColCenter stat">
+                        <span>
+                            <CountUp start={8800} end={9000}/>
+                        <span>+</span>
+                        </span>
+                        <span className='secondaryText'>Premium products</span>
+                    </div>
+
+                    <div className="flexColCenter stat">
+                        <span>
+                            <CountUp start={1950} end={2000}/>
+                            <span>+</span>
+                        </span>
+                        <span className='secondaryText'>Happy Customers</span>
+                    </div>
+
+                    <div className="flexColCenter stat">
+                        <span>
+                            <CountUp  end={20}/>
+                            <span>+</span>
+                        </span>
+                        <span className='secondaryText'>Award Winnings</span>
+                    </div>
+                </div>
+            </div>
+
+            <div className="hero-right">
+                <div className="image-container">
+                    <img src="./hero-image.png" alt="" />
+                </div>
+            </div>
+            </div>
+           
+       
+    </section>
+  )
+}
+
+export default Hero
